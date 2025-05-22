815933e02c40c8e001f84f88d1955907ac735b13
gsycl2004 <gsycl2004@gmail.com>
2025-05-22 20:20:47 +0800

    项目创建和初始化

---
+++ src/main/java/me/myot233/booksystem/BookSystemApplication.java
@@ -0,0 +1,13 @@
+package me.myot233.booksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookSystemApplication.class, args);
+    }
+
+}
